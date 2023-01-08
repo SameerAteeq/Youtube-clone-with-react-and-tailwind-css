2a46572d6696f9dce59ddf7d96900260aec6c2c3
SameerAteeq <sameerateeq0@gmail.com>
2023-01-08 15:13:40 +0500

    change

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://youtube138.p.rapidapi.com'
 const options = {
     params: { hl: 'en', gl: 'US' },
     headers: {
-        'X-RapidAPI-Key': '93f818508bmsh2f5b42419347cfep129bdcjsn210fa5c51862',
+        'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
         'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
 }
